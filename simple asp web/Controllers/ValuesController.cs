3aaa57a1f821d5329e65f6fdb5d84be2661bc2de
Working Hard <Working Hard>
2016-11-17 23:44:07 +0200

    routing for crud

--- simple asp web/Controllers/ValuesController.cs
+++ simple asp web/Controllers/ValuesController.cs
@@ -15,31 +15,44 @@ using System.Web.Mvc;
 
 namespace simple_asp_web.Controllers
 {
+    //Download Telerik Fiddler Web Debugger to check http requests results
     //[Authorize]
     public class ValuesController : ApiController
     {
-        //Get api/values
+        //Determining Generic class strings
+        static List<string> strings = new List<string>()
+        {
+            "value0","value1", "value2"
+        };
+
+        //GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] {"value1", "value2" };
+            return strings;
         }
-
+        
+        //GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return strings[id];
         }
-
-        public void Post([FromBody] string value) {
-
+        
+        //POST api/values
+        public void Post([FromBody] string value)
+        {
+            strings.Add(value);
         }
-
+        
+        //PUT api/values/5
         public void Put(int id, [FromBody] string value)
         {
-
+            strings[id] = value;
         }
 
+        //DELETE api/values/5
         public void Delete(int id)
         {
+            strings.RemoveAt(id);
         }
         // GET: Values
         //public ActionResult Index()
